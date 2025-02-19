9e8d54098065befb8cf1c546ea013d62474fbe22
Svyatoslav <ssn.24@uni-dubna.ru>
2025-02-20 00:07:41 +0300

    Add files via upload

---
+++ Task4.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class Task2
+{
+    public static int task2(string number)
+    {
+        bool flag = true;
+
+        char firstDigit = number[0];
+        
+        for (int i = 1; i < number.Length; i++)
+        {
+            if (number[i] != firstDigit)
+            {
+                flag = false;
+                break; 
+            }
+        }
+
+        // Выводим результат
+        if (flag)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    
+    }
+
+    public static void test2()
+    {
+        if (task2("222") == 1)
+        {
+            Console.WriteLine("OK");
+        }
+        else
+        {
+            Console.WriteLine("FAIL");
+        }
+
+        if (task2("123") == 0)
+        {
+            Console.WriteLine("OK");
+        }
+        else
+        {
+            Console.WriteLine("FAIL");
+        }
+
+        if (task2("999999999999999999") == 1)
+        {
+            Console.WriteLine("OK");
+        }
+        else
+        {
+            Console.WriteLine("FAIL");
+        }
+    }
+
+    static void 
+        Main()
+    {
+        test2();
+    }
+}
