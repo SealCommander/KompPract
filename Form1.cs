78aa12fc33200008b7ee35a92ed61a7aca079fab
Svyatoslav <ssn.24@uni-dubna.ru>
2025-02-20 21:39:28 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private int num_rand;
+        public Form1()
+        {
+            InitializeComponent();
+            Random rand = new Random();
+            num_rand = rand.Next(0, 100);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(textBox1.Text);
+            if (num > num_rand)
+            {
+                label1.Text = "Меньше";
+            }
+            else if (num < num_rand)
+            {
+                label1.Text = "Больше";
+            }
+            else
+            {
+                label1.Text = "Ура";
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
